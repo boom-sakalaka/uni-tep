6c42c9bf78ff4936a036e2f7099414b2f0562fc3
guzhihao <guzhihao@pengzhirui.com>
2024-05-18 11:40:37 +0800

    feat: 配置eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    eslint({
+      emitWarning: true,
+      emitError: true,
+    }),
+  ],
 });
